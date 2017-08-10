56aaadb585c489a2e1926fa65c6519ca7de2ff80
bo.li <bo.li@SZIMLIBO.transsion.com>
2017-08-10 19:34:35 +0800

    8月10号题目

---
+++ src/y2017/m8/d2/Main.java
@@ -0,0 +1,10 @@
+package y2017.m8.d2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/y2017/m8/d2/Test1434.java
@@ -0,0 +1,10 @@
+package y2017.m8.d2;
+
+public class Test1434 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
